fdd38a2b611635139a7d692c331a4a502b36685e
jk <jkty94@gmail.com>
2024-09-26 21:31:36 +0900

    first commit

---
+++ src/main/kotlin/com/example/subway_line/SubwayLineApplication.kt
@@ -0,0 +1,11 @@
+package com.example.subway_line
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SubwayLineApplication
+
+fun main(args: Array<String>) {
+	runApplication<SubwayLineApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/subway_line/SubwayLineApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.subway_line
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SubwayLineApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
